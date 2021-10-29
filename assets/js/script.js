adfb9374a8297fb00df0093a5317e8012df02f59
mcp321 <m_cp3@yahoo.com>
2021-10-29 16:43:03 +0000

    added subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    }    else {
+    }  else if (gameType === "subtract"){
+        displaySubstractionQuestion(num1, num2);
+    }  else {
         alert (`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -72,7 +74,9 @@ function calculateCorrectAnswer(){
     if(operator === "+"){
         return [operand1 + operand2, "addition"];
     } else if(operator ==="x"){
-        return [operand1 * operand2, "multily"]
+        return [operand1 * operand2, "multiply"]
+    } else if (operator === "-"){
+        return[operand1 - operand2,"subtract"];
     } else{
         alert (`Uniplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -109,7 +113,10 @@ function displayAdditionQuestion(operand1, operand2){
 }
 
 
-function displaySubstractionQuestion(){
+function displaySubstractionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1: operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2: operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
