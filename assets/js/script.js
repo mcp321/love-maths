bac19ac731e718ddff296de53c49c5280a4d91e4
mcp321 <m_cp3@yahoo.com>
2021-10-31 09:16:55 +0000

    added division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     }  else if (gameType === "subtract"){
         displaySubstractionQuestion(num1, num2);
-    }  else {
+    }  else if (gameType === "division"){
+        displayDivisionQuestion(num1, num2);
+    }   else {
         alert (`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -85,10 +87,13 @@ function calculateCorrectAnswer(){
     if(operator === "+"){
         return [operand1 + operand2, "addition"];
     } else if(operator ==="x"){
-        return [operand1 * operand2, "multiply"]
+        return [operand1 * operand2, "multiply"];
     } else if (operator === "-"){
         return[operand1 - operand2,"subtract"];
-    } else{
+    } else if (operator === "/"){
+        return[operand1 / operand2, "division"];
+    }
+        else{
         alert (`Uniplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -137,4 +142,11 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2){
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1: operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2: operand1;
+    document.getElementById('operator').textContent = "/";
 }
